a23646615a82b9ba796475c72a0c81d9cc9ae90a
Mousikar <3029606296@qq.com>
2025-09-16 19:54:12 +0800

    tb3 files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mr/renmeng/ws_swarm_robot_py/src
+# Build directory: /home/mr/renmeng/ws_swarm_robot_py/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_swarm_robot_tb3")
+subdirs("turtlebot3_description")
